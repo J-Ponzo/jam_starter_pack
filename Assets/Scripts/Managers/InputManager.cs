65ea980b22021b9e15c65e950f7bbb2bcdd2aaca
jponzo <jponzo@wanadev.fr>
2019-05-28 14:02:25 +0200

    InputManager() & OptionManager WIP (not tested implementations)

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -9,8 +9,8 @@ public class InputManager : Singleton<InputManager>
 
     public enum AKey
     {
-        Select,
-        Back,
+        Submit,
+        Cancel,
         Start
     }
 
@@ -20,25 +20,50 @@ public class InputManager : Singleton<InputManager>
         VMove
     }
 
-    private Dictionary<AKey, KeyCode> keyBindings;
-    private Dictionary<AAxis, string> axisBindings;
+    private KeyCode[] validKeyCodes = (KeyCode[]) Enum.GetValues(typeof(KeyCode));
+
+    private Dictionary<AKey, KeyCode> keyBindings = new Dictionary<AKey, KeyCode>();
+    private Dictionary<AAxis, string> axisBindings = new Dictionary<AAxis, string>();
 
     private void Start()
     {
+
+        LoadBindings();
+    }
+
+    private void Update()
+    {
+        KeyCode code = GetCurrentKey();
+        if (code != KeyCode.None)
+        {
+            Debug.Log(code);
+        }
+    }
+
+    private void LoadBindings()
+    {
+        keyBindings.Clear();
+        axisBindings.Clear();
+
         foreach (string name in Enum.GetNames(typeof(AKey)))
         {
-            string keyCode_str = OptionsManager.Instance.GetString(name + "_KeyBinging");
-            KeyCode keyCode = (KeyCode) Enum.Parse(typeof(KeyCode), keyCode_str);
+            string keyCode_str = OptionsManager.Instance.GetString(name + "_KeyBinding");
+            KeyCode keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), keyCode_str);
             AKey aKey = (AKey)Enum.Parse(typeof(AKey), name);
             keyBindings.Add(aKey, keyCode);
         }
 
         foreach (string name in Enum.GetNames(typeof(AAxis)))
         {
-            string axis_str = OptionsManager.Instance.GetString(name + "_AxisBinging");
+            string axis_str = OptionsManager.Instance.GetString(name + "_AxisBinding");
             AAxis aAxis = (AAxis)Enum.Parse(typeof(AAxis), name);
             axisBindings.Add(aAxis, axis_str);
         }
+    }
+
+    public void SaveBindings()
+    {
+        OptionsManager.Instance.Save();
     }
 
     public float GetAxis(AAxis aAxis)
@@ -59,5 +84,17 @@ public class InputManager : Singleton<InputManager>
     public bool GetKey(AKey aKey)
     {
         return Input.GetKey(keyBindings[aKey]);
+    }
+
+    private  KeyCode GetCurrentKey()
+    {
+        foreach (KeyCode keyCode in validKeyCodes)
+        {
+            if (Input.GetKey(keyCode))
+            {
+                return keyCode;
+            }
+        }
+        return KeyCode.None;
     }
 }
--- Assets/Scripts/Managers/OptionsManager.cs
+++ Assets/Scripts/Managers/OptionsManager.cs
@@ -7,8 +7,52 @@ public class OptionsManager : Singleton<OptionsManager>
 {
     protected OptionsManager() { }
 
-    internal string GetString(string v)
+    public StringOption[] stringPrefs;
+
+    [Serializable]
+    public struct StringOption
     {
-        throw new NotImplementedException();
+        public string key;
+        public string defaultValue;
+    }
+
+    public string GetDefaultString(string key)
+    {
+        string defaultValue = null;
+        foreach (StringOption strOption in stringPrefs)
+        {
+            if (strOption.key == key) defaultValue = strOption.defaultValue;
+        }
+
+        return defaultValue;
+    }
+
+    public string GetString(string key)
+    {
+        string defaultValue = GetDefaultString(key);
+        if (defaultValue == null)
+        {
+            Debug.LogError("Try retrieving string " + key + " from preferences but this key is not registered");
+            return null;
+        }
+
+        return PlayerPrefs.GetString(key, defaultValue);
+    }
+
+    public void SetString(string key, string value)
+    {
+        string defaultValue = GetDefaultString(key);
+        if (defaultValue == null)
+        {
+            Debug.LogError("Try setting string " + key + " in preferences but this key is not registered");
+            return;
+        }
+
+        PlayerPrefs.SetString(key, value);
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.Save();
     }
 }
