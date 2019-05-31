910d5d4df94fc19eaf7567794263a83b7f8eb377
jponzo <jponzo@wanadev.fr>
2019-05-31 17:40:25 +0200

    DEBUGBehaviour WIP

---
+++ Assets/Scripts/Utils/DEBUGBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUGBehaviour : MonoBehaviour
+{
+    public string owner;
+
+    private void Awake()
+    {
+        bool selfDestroy = true;
+#if UNITY_EDITOR
+    if (SystemInfo.deviceName == owner)
+    {
+
+    }
+#else
+    selfDestroy = true;
+#endif
+    }
+}
